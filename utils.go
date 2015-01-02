aeb4292919c3af712ae49c909c8d4d8ba2ccb092
Aron Fyodor Asor <aronasorman@gmail.com>
2015-01-02 13:02:33 -0800

    solve set1 challenge 2

--- utils.go
+++ utils.go
@@ -8,7 +8,7 @@ import (
 
 func Xor(str1, str2 string) string {
 	str1base10, _ := hex.DecodeString(str1)
-	str2base10, _ := hex.DecodeString(str1)
+	str2base10, _ := hex.DecodeString(str2)
 
 	var out bytes.Buffer
 	for i, _ := range str1base10 {
@@ -16,24 +16,28 @@ func Xor(str1, str2 string) string {
 		out.WriteByte(byte(b))
 	}
 
-	return out.String()
+	return hex.EncodeToString(out.Bytes())
+}
+
+func ToBase64(base10 []byte) (string, error) {
+	var outb bytes.Buffer
+
+	encoder := base64.NewEncoder(base64.StdEncoding, &outb)
+	defer encoder.Close()
+
+	_, err := encoder.Write(base10)
+	if err != nil {
+		return "", err
+	}
+
+	return outb.String(), nil
 }
 
 func Hex2Base64(base16str string) (string, error) {
 	base10, err := hex.DecodeString(base16str)
 	if err != nil {
 		return "", err
+	} else {
+		return ToBase64(base10)
 	}
-
-	var outb bytes.Buffer
-
-	encoder := base64.NewEncoder(base64.StdEncoding, &outb)
-	defer encoder.Close()
-
-	_, err = encoder.Write(base10)
-	if err != nil {
-		return "", err
-	}
-
-	return outb.String(), nil
 }
